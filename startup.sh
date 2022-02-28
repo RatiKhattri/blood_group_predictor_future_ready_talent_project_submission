7a56f9a143902b6899237b262f66bf4dde04c101
rati khatttri <91478688+RatiKhattri@users.noreply.github.com>
2022-02-28 22:34:50 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,8 +2,3 @@ gunicorn --bind=0.0.0.0 --timeout 600 blood_group_predictor:myapp
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir myapp website:app
 from blood_group_predictor_app.webapp import app
 gunicorn --bind=0.0.0.0 --workers=4 startup:app
-py -3 -m venv .env
-.env\scripts\activate
-pip install -r requirements.txt
-$env:FLASK_APP = "blood_group_predictor.py"
-python -m flask run
