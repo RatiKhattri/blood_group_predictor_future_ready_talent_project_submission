e3a0388eb486266b0804cb704aa9d45fe2a6a009
rati khatttri <91478688+RatiKhattri@users.noreply.github.com>
2022-03-02 00:11:52 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,4 +0,0 @@
-gunicorn --bind=0.0.0.0 --timeout 600 blood_group_predictor:myapp
-gunicorn --bind=0.0.0.0 --timeout 600 --chdir myapp website:app
-from blood_group_predictor_app.webapp import app
-gunicorn --bind=0.0.0.0 --workers=4 startup:app
